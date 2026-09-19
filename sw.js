cf9ed0f4ec96d72bc079ae13a6886de6254caac5
ankushjavadev <ankush.javadev@gmail.com>
2026-09-19 09:56:18 +0530

    Add files via upload
    New updated version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* अपनी हट्टी — app shell cache. Bump VERSION after every edit to index.html. */
-const VERSION = 'ah-v11';
+const VERSION = 'ah-v12';
 const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
